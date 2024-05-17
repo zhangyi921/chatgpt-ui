664cbc2df4a25e3301612f6b803b2d8ae0a09340
Yi <yi16@ualberta.ca>
2024-05-17 14:50:52 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Markdown from 'react-markdown'
 import './App.css'
 import OpenAI from 'openai'
